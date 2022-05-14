cbb02cff0782df7f6e3ba9097c3125389a2203b3
vbth7777 <vbth7777@gmail.com>
2022-05-15 06:08:01 +0700

    Update 15/5/2022

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,17 @@
+#------------------Import Modules-----------------
+Import-Module Terminal-Icons
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+#------------------Configure-----------------
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+set-alias gg googler
